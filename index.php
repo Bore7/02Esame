73f346b56a2ca85d37f319162d9da6c40ba4b06c
Bore <privatobore@gmail.com>
2024-02-07 17:38:32 +0100

    Trasformato il contenuto di header da css in scss

---
+++ index.php
@@ -0,0 +1,467 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Sito Web Personale">
+        <title>Sito Web Personale</title> 
+        <link href="css/stile.css"  rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- link libreria per le icone social-->
+        
+
+    </head>
+
+    <body>
+        
+        <header class="header">
+
+            <a href="#" class="logo"><img src="img/Logo-V.png" alt="Logo V.V" title="V.V" height="80" width="80"></a>
+
+            <nav class="BarNav">
+
+                <a href="#Home" class="active" >Home</a>
+                <a href="#AboutMe">About me</a>
+                <a href="#Portfolio">Portfolio</a>
+                <a href="#Contatti">Contatti</a>
+
+            </nav>
+
+        </header>
+        
+        <video autoplay loop muted playsinline class="Background-video">
+            <source src="Background-video.mp4" type="video/mp4"> 
+                
+        </video>
+        
+      
+
+        <section class="Home" id="Home">
+            
+            <div class="Contenuto-home">
+                <h3>Full-Stack Developer</h3>
+                <div class="TitoloNome"><h1>Valerio </h1>
+                    <h1> Vignali</h1></div>
+            
+                
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+                    laborum."
+                </p>
+            </div>
+            <div class="Logo-Home">
+                <a href="#" class="sfondologo"><img src="img/Rombo-sfondo-logo.png" alt="sfondologo" title="sfondologo" class="rombo"><img src="img/Logo-V.png" alt="Logo V.V" title="V.V" class="logoVV" ></a>
+    
+
+
+            </div>
+
+
+        </section>
+
+
+        <section class="About-me" id="AboutMe">
+
+            <div><img src="img/Programmatore.jpg" alt="Programmatore" title="About Me" width="700" height="1050" class="Programimg"> </div>
+
+
+            
+            <div class="Contenuto-Aboutme">
+                
+                <div > <h2>About Me</h2></div>
+                
+                
+                <div> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+                    laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                    dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+                    laborum.
+                </p></div>
+
+            </div>
+        </section>
+
+
+        <section class="Servizi">
+
+
+
+            
+            <div class="servizi">
+                <div class="title">
+                    <h2>Servizi</h2>
+                </div>
+
+                <div class="box">
+                    <div class="servizio">
+                        
+                        <span class="material-symbols-outlined">
+                            home
+                        </span>
+                        <h5>Web Designer</h5>
+
+                        <div class="Descrizione">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                                .</p>
+                            
+                            <p style="text-align: center;">
+                            <a class="button" href="#">Read More</a></p>
+                        </div>
+
+
+                    </div>
+
+                    <div class="servizio">
+                        <span class="material-symbols-outlined">
+                            home
+                        </span>
+                        <h5>Web Master</h5>
+
+                        <div class="Descrizione">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                            </p>
+                            
+                            <p style="text-align: center;">
+                            <a class="button" href="#">Read More</a></p>
+                        </div>
+
+                    </div>
+
+                    <div class="servizio">
+                        <span class="material-symbols-outlined">
+                            home
+                        </span>
+                        <h5>Frontend Developer</h5>
+
+                        <div class="Descrizione">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                               </p>
+                            
+                            <p style="text-align: center;">
+                            <a class="button" href="#">Read More</a></p>
+                        </div>
+
+
+                    </div>
+
+
+                    <div class="servizio">
+                        <span class="material-symbols-outlined">
+                            home
+                        </span>
+                        <h5>Backend Developer</h5>
+
+
+                        <div class="Descrizione">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                                </p>
+                            
+                            <p style="text-align: center;">
+                            <a class="button" href="#">Read More</a></p>
+                        </div>
+
+
+                    </div>
+
+
+                    <div class="servizio">
+                        <span class="material-symbols-outlined">
+                            home
+                        </span>
+                        <h5>Full-Stack Developer</h5>
+
+                        <div class="Descrizione">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  
+                                </p>
+                            
+                            <p style="text-align: center;">
+                            <a class="button" href="#">Read More</a></p>
+                        </div>
+
+                        
+
+
+                    </div>
+                </div>
+
+
+                    
+                    
+                
+            </div>
+        </section>
+
+        <!-- Sezione Lavori e Progetti -->
+
+        <section class="Lavori" id="Portfolio">
+            <div class="container">
+                <div class="Titolo-sezione-prj">
+                    <h2 class="Quality-works-Progetti-Recenti">Quality works & Progetti Recenti</h2>
+                </div>
+                <h6><i>Ruolo nel progetto:</i></h6>
+            </div>
+
+            <div class="Bottoni-Lavori">  <!--quando imparo il jss farò in modo di poter cambiare l 'ordine dei lavori in base al ruolo svolto in esso'-->
+                <button type="button" class="active">All</button>
+                <button type="button" >Web Design </button>
+                <button type="button" >Web Master </button>
+                <button type="button" >Backend Developer </button>
+                <button type="button" >Fontend Developer </button>
+                <button type="button" >Fullstack Developer </button>
+                
+            </div>
+
+            <div class="row-grid">
+                <div class="Riga1">
+                    <div class="Rigadiv">
+                        <div class="img">
+                            <a href="Pagina-Progetto.html"><img src="img/PortfolioPrj/pj2.jpg" alt="pj2" title="pj2" class="pjimg"></a>
+                            <div class="Titolo-pj">
+                                <h4>Progetto 1</h4>
+                                <span class="Testo-secondario">Web Design</span>
+                            </div>
+                        </div>
+                       
+
+                        <div class="img">
+                            <img src="img/PortfolioPrj/pj3.jpg" alt="pj2" title="pj2" class="pjimg">
+                            <div class="Titolo-pj">
+                                <h4>Titolo pj</h4>
+                                <span class="Testo-secondario">Web Master</span>
+                            </div>
+                        </div>
+
+                        <div class="img">
+                            <img src="img/PortfolioPrj/pj4.jpg" alt="pj4" title="pj4" class="pjimg">
+                            <div class="Titolo-pj">
+                                <h4>Titolo pj</h4>
+                                <span class="Testo-secondario">Frontend Developing</span>
+                            </div>
+                        </div>
+
+                        
+                       
+                    </div>
+
+                    
+                    
+                </div>
+
+                <div class="Riga2">
+                    <div class="Rigadiv">
+                        <div class="img">
+                            <img src="img/PortfolioPrj/pj2.jpg" alt="pj2" title="pj2" class="pjimg">
+                            <div class="Titolo-pj">
+                                <h4>Titolo pj</h4>
+                                <span class="Testo-secondario">Web Design</span>
+                            </div>
+                        </div>
+                       
+
+                        <div class="img">
+                            <img src="img/PortfolioPrj/pj3.jpg" alt="pj2" title="pj2" class="pjimg">
+                            <div class="Titolo-pj">
+                                <h4>Titolo pj</h4>
+                                <span class="Testo-secondario">Web Master</span>
+                            </div>
+                        </div>
+
+                        <div class="img">
+                            <img src="img/PortfolioPrj/pj4.jpg" alt="pj4" title="pj4" class="pjimg">
+                            <div class="Titolo-pj">
+                                <h4>Titolo pj</h4>
+                                <span class="Testo-secondario">Frontend Developing</span>
+                            </div>
+                        </div>
+
+                        
+                       
+                    </div>
+
+
+
+                    
+
+                    
+                    
+                </div>
+
+                <div class="Riga3">
+                    <div class="Rigadiv">
+                        <div class="img">
+                            <img src="img/PortfolioPrj/pj2.jpg" alt="pj2" title="pj2" class="pjimg">
+                            <div class="Titolo-pj">
+                                <h4>Titolo pj</h4>
+                                <span class="Testo-secondario">Web Design</span>
+                            </div>
+                        </div>
+                       
+
+                        <div class="img">
+                            <img src="img/PortfolioPrj/pj3.jpg" alt="pj2" title="pj2" class="pjimg">
+                            <div class="Titolo-pj">
+                                <h4>Titolo pj</h4>
+                                <span class="Testo-secondario">Web Master</span>
+                            </div>
+                        </div>
+
+                        <div class="img">
+                            <img src="img/PortfolioPrj/pj4.jpg" alt="pj4" title="pj4" class="pjimg">
+                            <div class="Titolo-pj">
+                                <h4>Titolo pj</h4>
+                                <span class="Testo-secondario">Frontend Developing</span>
+                            </div>
+                        </div>
+
+                        
+                       
+                    </div>
+
+
+
+                    
+
+                    
+                    
+                </div>
+
+
+               
+            </div>
+
+
+            
+
+            
+
+
+
+        </section>
+
+
+        <!-- Sezione Compilazione form-->
+        <section class="form">
+            <div class="Titolo-Form">
+                <h1>Compila per richiedere informazioni </h1>
+                <p>Inserisci i tuoi contatti</p>
+
+            </div>
+
+            <form action="http://www.example.com/subscribe.php" method="get">
+
+                <div class="Nome-Cognome">
+
+                
+               
+                    <input type="text" name="nome" size="15"  placeholder="Nome" required>
+                    
+                   
+                    <input type="text" name="cognome" size="15"  placeholder="Cognome">
+                    
+                
+
+
+                </div>
+
+                <div class="Tel-Email">
+
+                
+               
+                    <input type="tel" name="telefono" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"  placeholder="Telefono">
+                    
+                   
+                    <input type="email" name="email"  placeholder="Indirizzo E-mail">
+                    
+                
+
+
+                </div>
+
+                <div class="Textarea">
+
+                
+                    <textarea name="Messaggio" cols="20" rows="4" placeholder="Scrivi il tuo messaggio"></textarea>
+                
+                    
+                    
+                </div>
+
+                <div class="Submit-button">
+                    
+                    <input type="submit" value="Invia">
+                
+                </div>
+            </form>
+
+           
+    
+
+        </section>
+
+
+
+        <footer>
+
+
+            <a href="#" class="logo"><img src="img/Logo-V.png" alt="Logo V.V" title="V.V" height="80" width="80"></a>
+    
+     
+            <div class="footer">
+               
+                <div class="Recapiti">
+                    <p id="Contatti">Contatti</p>
+                    <ul>
+                        <li>via Stazzo quadro 7, 00060 Riano</li>
+                        <li><a href="mailto:valeriovign@outlook.com">valeriovign@outlook.com</a></li>
+                        <li><a href="tel:+393319240001">+39 3318916653</a></li>
+                    </ul>
+                </div>
+    
+                <div class="ContattamiSu">
+        
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-linkedin"></a>
+                    <a href="#" class="fa fa-google"></a>
+    
+                
+    
+    
+                    
+                   
+                </div>
+    
+               
+            </div>
+            
+    
+    
+    
+        </footer>
+    
+
+    </body>
+
+  
+</html>
