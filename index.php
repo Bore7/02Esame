cd9c0765325a3e63acc58e17f70bc46ab5dfd18b
Bore <privatobore@gmail.com>
2024-02-07 18:52:27 +0100

    Aggiunto file index.php e trasferito il codice HTML dal file precedente

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Sito Web Personale">
         <title>Sito Web Personale</title> 
-        <link href="css/stile.css"  rel="stylesheet">
+        <link href="../Progetto/css/stile.scss"  rel="stylesheet">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- link libreria per le icone social-->
         
